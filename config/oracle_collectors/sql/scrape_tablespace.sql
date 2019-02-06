2c06e8d7eb388641e01b810eb95aecb6ffc58b56
Aaron Reinard <dks0408070@dcsg.com>
2019-02-06 15:28:15 -0500

    updated oracle metrics

--- config/oracle_collectors/sql/scrape_tablespace.sql
+++ config/oracle_collectors/sql/scrape_tablespace.sql
@@ -1,66 +1,26 @@
-SELECT
-  Z.name AS "name",
-  dt.status,
-  dt.contents,
-  dt.extent_management,
-  Z.bytes AS "bytes",
-  Z.max_bytes  AS "max_bytes",
-  Z.free_bytes AS "free_bytes"
-FROM
-(
-    SELECT 
-        ddf.tablespace_name AS name,
-        SUM(ddf.bytes) AS bytes,
-        SUM(nvl(dfs.free_bytes,0)) AS free_bytes,
-        SUM(ddf.max_bytes) AS max_bytes
-    FROM (
-        SELECT 
-            tablespace_name,
-            bytes,
-            CASE
-                WHEN maxbytes = 0 
-                THEN bytes
-                ELSE maxbytes
-                END AS max_bytes
-        FROM sys.dba_data_files) ddf
-    INNER JOIN (
-        SELECT 
-            tablespace_name,
-            sum(bytes) as free_bytes
-        FROM sys.dba_free_space
-        GROUP BY tablespace_name) dfs
-    ON ddf.tablespace_name = dfs.tablespace_name
-    GROUP BY
-        ddf.tablespace_name
-  UNION ALL
-  SELECT
-    Y.name                   as name,
-    MAX(nvl(Y.free_bytes,0)) as free_bytes,
-    SUM(Y.bytes)             as bytes,
-    SUM(Y.max_bytes)         as max_bytes
-  FROM
+SELECT    
+    a.TABLESPACE_NAME AS "name",
+    a.BYTES AS "bytes_used",
+    b.BYTES AS "bytes_free",
+    a.bytes+b.bytes AS "max_bytes",
+    b.largest AS "largest_data_file",
+    round(((a.BYTES-b.BYTES)/a.BYTES)*100,2) percent_used
+FROM    
     (
-      SELECT
-        dtf.tablespace_name as name,
-        dtf.status as status,
-        dtf.bytes as bytes,
-        (
-          SELECT
-            ((f.total_blocks - s.tot_used_blocks)*vp.value)
-          FROM
-            (SELECT tablespace_name, sum(used_blocks) tot_used_blocks FROM gv$sort_segment WHERE  tablespace_name!='DUMMY' GROUP BY tablespace_name) s,
-            (SELECT tablespace_name, sum(blocks) total_blocks FROM dba_temp_files where tablespace_name !='DUMMY' GROUP BY tablespace_name) f,
-            (SELECT value FROM v$parameter WHERE name = 'db_block_size') vp
-          WHERE f.tablespace_name=s.tablespace_name AND f.tablespace_name = dtf.tablespace_name
-        ) as free_bytes,
-        CASE
-          WHEN dtf.maxbytes = 0 THEN dtf.bytes
-          ELSE dtf.maxbytes
-        END as max_bytes
-      FROM
-        sys.dba_temp_files dtf
-    ) Y
-  GROUP BY Y.name
-) Z, sys.dba_tablespaces dt
-WHERE
-  Z.name = dt.tablespace_name
+        SELECT     
+            TABLESPACE_NAME,
+            sum(BYTES) BYTES
+        FROM     dba_data_files
+        GROUP     by TABLESPACE_NAME
+    )
+    a,
+    (
+        select     TABLESPACE_NAME,
+            sum(BYTES) BYTES ,
+            max(BYTES) largest
+        from     dba_free_space
+        group     by TABLESPACE_NAME
+    )
+    b
+WHERE     a.TABLESPACE_NAME=b.TABLESPACE_NAME
+ORDER     by ((a.BYTES-b.BYTES)/a.BYTES) desc;
